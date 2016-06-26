513ccf9351294cbf2d76b419540eb7ba00560fca
Tiger <574240230@qq.com>
2016-06-27 00:49:01 +0900

    Add ex1,2,3

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment ,this is so you can read your program lager
+# Anything after the
+# is ignored by python.
+print "I could have code like this."# and the comment after is ignored
+
+# You can also user a comment to "disable" or comment out a piect of code:
+# print "this won't run."
+
+print "this will run."
