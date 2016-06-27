dc82a0bc02b94d9067e9e6685133f5b5b3ecad65
Tiger <574240230@qq.com>
2016-06-27 12:03:11 +0900

    add ex13

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second,third = argv
+
+print "The scropt is called:",script
+print "Your First variable is :",first
+print "Your Second variable is :",second
+print "Your Third variable is :",third
