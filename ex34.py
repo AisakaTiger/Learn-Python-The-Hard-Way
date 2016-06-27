e3359f0a49a12240b90c5574537572d7c474d68f
Tiger <574240230@qq.com>
2016-06-27 17:05:56 +0900

    add ex34

---
+++ ex34.py
@@ -0,0 +1,17 @@
+animals = ['bear', 'python', 'peacock', 'kangraoo', 'whale', 'platypus']
+
+print "The animal at 1 is :", animals[1]
+
+print "The 3rd animal is at index 2 :",animals[2]
+
+print "The 1st animal is at index 0 :",animals[0]
+
+print "The animal at 3 is :",animals[3]
+
+print "The 5th animal is at index 4 :",animals[4]
+
+print "The animal at 2 is :",animals[2]
+
+print "The 6th animal is at index 5 :",animals[5]
+
+print "The animal at 4 is :", animals[4]
